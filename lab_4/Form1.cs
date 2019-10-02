4c4840ba092ee809034020604a66983ae0bf112c
Сталин Мифодий <avatar226276@gmail.com>
2019-10-02 20:24:45 +0300

    DONE! TOCHNO!

--- lab_4/Form1.cs
+++ lab_4/Form1.cs
@@ -14,28 +14,31 @@ namespace lab_4
         private void btn_FreeThread_Click(object sender, EventArgs e)
         {
             PriorityTesting[] inst = new PriorityTesting[5]
-                { new PriorityTesting(), new PriorityTesting(), new PriorityTesting(), new PriorityTesting(), new PriorityTesting() };
-            Thread[] threads = new Thread[5];
-            Int32 thrID = Thread.CurrentThread.ManagedThreadId;
+                { new PriorityTesting(), new PriorityTesting(), new PriorityTesting(), new PriorityTesting(), new PriorityTesting() }; // Объявляем и инициализируем массив типа PriorityTesting 
+            Thread[] threads = new Thread[5]; //Объявляем массив потоков 
 
             for (int n = 0; n < 5; n++)
             {
-                threads[n] = new Thread(new ParameterizedThreadStart(inst[n].ThreadMethod));
+                threads[n] = new Thread(new ParameterizedThreadStart(inst[n].ThreadMethod)); // Делегируем экземплярный метод из нашего типа в поток
             }
 
+            //Задаем приоритеты
             threads[0].Priority = ThreadPriority.Lowest;
             threads[1].Priority = ThreadPriority.BelowNormal;
             threads[2].Priority = ThreadPriority.Normal;
             threads[3].Priority = ThreadPriority.AboveNormal;
             threads[4].Priority = ThreadPriority.Highest;
 
-            for (int i = 0; i < 5; i++)
+            //Поочерёдно стартуем потоки
+            foreach (Thread thr in threads)
             {
-                threads[i].Start(thrID);
+                thr.Start(thr.ManagedThreadId);
             }
 
+            //Даём главному потоку отдохнуть, он и так у нас бедный 
             Thread.Sleep(10000);
 
+            //Поочередно выводим информацию о потоках и кол-во выполненых операций 
             for (int i = 0; i < 5; i++)
             {
                 txt_debug.Text += $"ID потока: {threads[i].ManagedThreadId}\tЗначение счётчика: {inst[i].counter}\tПриоритет потока: {threads[i].Priority}\r\n";
--- lab_4/PriorityTesting.cs
+++ lab_4/PriorityTesting.cs
@@ -7,13 +7,13 @@ namespace lab_4
     {
         //public Boolean finish; Не нужен, так как есть реализованное в классе Thread экземплярное свойство
         
-        public Double counter = 0;
+        public Double counter = 0; // Экземплярный каунтер
 
         public void ThreadMethod(Object thrID)
         {
-            ProcessThread tempThr = Process.GetCurrentProcess().Threads[(Int32)thrID];
+            ProcessThread tempThr = Process.GetCurrentProcess().Threads[(Int32)thrID]; // Находим наш поток через все потоки исполняемого программой процесса 
 
-            while (tempThr.ThreadState != ThreadState.Terminated)
+            while (tempThr.ThreadState != ThreadState.Terminated) // Инкриментируем каунтер до завершения работы потока 
             {
                 counter++;
             }
